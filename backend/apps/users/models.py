3dd5eb8fb576dd0ccf2686eef57f89eecacb5d9a
Lucifer <SurdackiD1@gmail.com>
2026-03-11 16:33:18 +0100

    PZPP-23 added deleted_at, foreign key references

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -45,15 +45,18 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModel):
         is_superuser: bool
         created_at: datetime
         updated_at: datetime
+        deleted_at: datetime | None
 
+    
     email = models.EmailField(unique=True, db_index=True)
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
+    
     objects = UserManager()
 
     def __str__(self) -> str:
@@ -62,6 +65,6 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModel):
 class UserProfile(BaseModel):
     id = models.BigAutoField(primary_key=True)
     userId = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    username = models.CharField(max_length=64)
+    username = models.CharField(max_length=64, unique=True)
     description = models.CharField(max_length=255)
     profilePicture = models.ImageField
--- backend/common/models.py
+++ backend/common/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    deleted_at = models.DateTimeField(null=True, default=None)
     class Meta:
         abstract = True
