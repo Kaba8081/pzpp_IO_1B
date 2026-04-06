ef078c2cfd3c548e0fa0dedf529769d0751b8fb2
Kacper Baum <Kaba8081@gmail.com>
2026-04-06 14:54:01 +0200

    PZPP-65 - Refactor WorldRoomsSerializer to simplify world validation

--- backend/apps/forum/world_rooms/serializers.py
+++ backend/apps/forum/world_rooms/serializers.py
@@ -1,18 +1,15 @@
 from rest_framework import serializers
 
 from apps.forum.world_rooms.models import WorldRooms
-from apps.forum.worlds.models import Worlds
 
 class WorldRoomsSerializer(serializers.ModelSerializer):
-    world_id = serializers.IntegerField(read_only=True, source='world_id')
-    
+    world_id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = WorldRooms
         fields = '__all__'
 
     def validate(self, attrs):
-        
         name = attrs.get('name', '')
         if not isinstance(name, str) or not name.strip():
             raise serializers.ValidationError({'name': 'Name is required and cannot be blank.'})
@@ -22,26 +19,20 @@ class WorldRoomsSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({'name': 'Name must be at most 64 characters.'})
         attrs['name'] = name
 
-        world_id = attrs.get('world_id')
-        world_object = None
-        if world_id is not None:
-            try:
-                world_object = Worlds.objects.get(pk=world_id)
-            except Worlds.DoesNotExist:
-                raise serializers.ValidationError({'world_id': 'World with given id does not exist.'})
-        
-        attrs['world_object'] = world_object
+        world_object = attrs.get('world')
+        if world_object is None:
+            raise serializers.ValidationError({'world': 'World is required.'})
         return attrs
 
     def create(self, validated_data):
-        world_object = validated_data.pop('world_object', None)
+        world_object = validated_data.get('world')
         if world_object is None:
             raise serializers.ValidationError({'world': 'World is required to create a room.'})
-       
+
         name = validated_data.get('name')
         if WorldRooms.objects.filter(world=world_object, name__iexact=name).exists():
-            raise serializers.ValidationError({'name': 'A room with this name already exists in this world.'})
+            raise serializers.ValidationError(
+                {'name': 'A room with this name already exists in this world.'}
+            )
 
-        return WorldRooms.objects.create(world=world_object, **validated_data)
-
-    
+        return WorldRooms.objects.create(**validated_data)
