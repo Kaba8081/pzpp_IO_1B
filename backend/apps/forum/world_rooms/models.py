35cbc4ee91826110fa07da346da42b7e1af8efd0
Dorian Surdacki <152281042+xKOKSUx@users.noreply.github.com>
2026-04-07 16:19:44 +0200

    Pzpp-65 (#28)
    * PZPP-65 Added manager and serializer to world rooms
    
    * PZPP-65 Added manager and serializer to world rooms
    
    * PZPP-65 Fixed validate and create methods
    
    * PZPP-65 - Refactor WorldRoomsSerializer to simplify world validation
    
    ---------
    
    Co-authored-by: Kacper Baum <Kaba8081@gmail.com>

--- backend/apps/forum/world_rooms/models.py
+++ backend/apps/forum/world_rooms/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from common.models import BaseModel
 from apps.forum.worlds.models import Worlds
+from apps.forum.world_rooms.managers import WorldRoomManager
 
 
 class WorldRooms(BaseModel):
@@ -8,3 +9,9 @@ class WorldRooms(BaseModel):
     world = models.ForeignKey(Worlds, on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
+
+    objects = WorldRoomManager()
+    all_objects = models.Manager()
+
+    def __str__(self) -> str:
+        return str(self.name)
