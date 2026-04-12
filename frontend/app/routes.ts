8d2e3d51f0a065f866f7d8230cae098d3153abec
MSI\orypi <orypinska@gmail.com>
2026-04-12 21:08:42 +0200

    PZPP-67 Sidebar adjusted (rounded-xl added to home button). First version of MainPage created

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, route, index } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"), // keep or remove if not needed
   route("world/:worldId", "routes/ChannelRoom.tsx"),
+  route("MainPage", "routes/MainPage.tsx"), // ścieżka do strony głównej
 ] satisfies RouteConfig;
