967e30ae7c12a6cbe6ca2b472c75144986ef7e0c
Kacper Baum <Kaba8081@gmail.com>
2026-04-19 15:49:48 +0200

    PZPP-76 - Code refactor

--- backend/apps/forum/world_room_messages/serializers.py
+++ backend/apps/forum/world_room_messages/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 from apps.forum.world_room_messages.models import WorldRoomMessages
 
 class WorldRoomMessagesSerializer(serializers.ModelSerializer):
-    room = serializers.IntegerField(read_only=True, source='room_id')
-    user = serializers.IntegerField(read_only=True, source='user_id')
+    room = serializers.IntegerField()
+    user_profile = serializers.IntegerField()
 
     class Meta:
         model = WorldRoomMessages
