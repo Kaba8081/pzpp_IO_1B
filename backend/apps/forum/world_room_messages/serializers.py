703c2507a6e82c7616e2439d99a65e2747df9478
Kacper Baum <Kaba8081@gmail.com>
2026-04-19 15:16:23 +0200

    PZPP-73 - Removed redundand checks, refactor

--- backend/apps/forum/world_room_messages/serializers.py
+++ backend/apps/forum/world_room_messages/serializers.py
@@ -6,46 +6,30 @@ from apps.forum.world_rooms.models import WorldRooms
 from apps.forum.world_user_profiles.models import WorldUserProfiles
 
 class WorldRoomMessagesSerializer(serializers.ModelSerializer):
-    room = serializers.IntegerField(read_only=True, source='room_id')
-    user = serializers.IntegerField(read_only=True, source='user_id')
+    room = serializers.PrimaryKeyRelatedField(queryset=WorldRooms.objects.all())
+    user_profile = serializers.PrimaryKeyRelatedField(queryset=WorldUserProfiles.objects.all())
 
     class Meta:
         model = WorldRoomMessages
         fields = '__all__'
 
-    def validate(self, data):
+    def validate(self, attrs):
+        user_profile_id = attrs.get('user_profile')
+        room_id = attrs.get('room')
 
-        content = data.get('content')
-        user_profile_id = data.get('user_profile')
-        room_id = data.get('room')
+        if user_profile_id:
+            try:
+                WorldUserProfiles.objects.get(id=user_profile_id)
+            except WorldUserProfiles.DoesNotExist as exc:
+                raise ValidationError("Invalid user profile.") from exc
 
-        if not content:
-            raise ValidationError("Content is required.")
+        if room_id:
+            try:
+                WorldRooms.objects.get(id=room_id)
+            except WorldRooms.DoesNotExist as exc:
+                raise ValidationError("Invalid room.") from exc
 
-        if len(content) > 2048:
-            raise ValidationError("Content cannot exceed 2048 characters.")
-
-        if not user_profile_id:
-            raise ValidationError("User profile is required.")
-
-        if not room_id:
-            raise ValidationError("Room is required.")
-
-
-        try:
-            user_profile = WorldUserProfiles.objects.get(id=user_profile_id)
-        except WorldUserProfiles.DoesNotExist:
-            raise ValidationError("Invalid user profile.")
-
-
-        try:
-            room = WorldRooms.objects.get(id=room_id)
-        except WorldRooms.DoesNotExist:
-            raise ValidationError("Invalid room.")
-
-
-        return data
+        return attrs
 
     def create(self, validated_data):
-        
         return WorldRoomMessages.objects.create(**validated_data)
