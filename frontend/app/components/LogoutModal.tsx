06a93d4a4d6be4fa66c6f9b8ca75ba4a3224391d
ctx3 <ctx4@proton.me>
2026-04-20 17:38:55 +0200

    minor adjustments

--- frontend/app/components/LogoutModal.tsx
+++ frontend/app/components/LogoutModal.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { LogOut, X } from 'lucide-react';
-import { useNavigate } from 'react-router'; 
+import React from "react";
+import { LogOut, X } from "lucide-react";
+import { useNavigate } from "react-router";
 import { useUserStore } from "@/stores/UserStore";
-import { Button } from './Button'; 
-import { Modal } from './Modal'; 
+import { Button } from "./Button";
+import { Modal } from "./Modal";
 
 export const LogoutModal = () => {
   const { logout, modal } = useUserStore();
@@ -12,14 +12,13 @@ export const LogoutModal = () => {
   const handleLogout = () => {
     logout();
     modal.close();
-    navigate('/'); 
+    navigate("/");
   };
 
   return (
     <Modal name="logout">
       <div className="flex flex-col items-center text-center p-6">
-        
-        <button 
+        <button
           onClick={modal.close}
           className="absolute top-4 right-4 text-white hover:text-primary transition-colors"
         >
@@ -28,9 +27,7 @@ export const LogoutModal = () => {
 
         <LogOut size={44} className="text-error mb-4" strokeWidth={1.5} />
 
-        <h2 className="font-cinzel text-md tracking-[0.2em] uppercase mb-6">
-          Logout
-        </h2>
+        <h2 className="font-cinzel text-md tracking-[0.2em] uppercase mb-6">Logout</h2>
 
         <p className="font-cinzel text-xs font-bold tracking-[0.1em] uppercase mb-8">
           Are you sure you want to logout of your account?
@@ -45,11 +42,7 @@ export const LogoutModal = () => {
             CANCEL
           </Button>
 
-          <Button
-            variant="outline"
-            onClick={handleLogout}
-            className="flex-1 text-white tracking-widest font-cinzel !border-error !bg-error transition-all duration-300"
-          >
+          <Button variant="danger" onClick={handleLogout} className="flex-1 tracking-widest">
             LOGOUT
           </Button>
         </div>
--- frontend/app/components/Modal.tsx
+++ frontend/app/components/Modal.tsx
@@ -19,12 +19,10 @@ export const Modal = ({ name, children }: ModalProps) => {
       onClick={modal.close}
     >
       <div
-        className="relative w-full max-w-2xl border border-primary border-2 transform overflow-hidden rounded-2xl bg-background p-6 shadow-2xl transition-all"
+        className="relative w-full max-w-2xl border-2 border-primary transform overflow-hidden rounded-2xl bg-background p-6 shadow-2xl transition-all"
         onClick={(e) => e.stopPropagation()}
       >
-        {/* ZAWARTOŚĆ MODALA */}
         <div className="mt-2 text-white">{children}</div>
-
       </div>
     </div>
   );
